66a65030160f981e7e70101ed6bd13c57e75565f
Mr.LeiDeSen <mrleidesen@outlook.com>
2020-10-14 18:33:47 +0800

    add games: 永恒之柱、异形隔离

--- src/mock/index.js
+++ src/mock/index.js
@@ -7,4 +7,20 @@ export const WALK_THROUGH = [
         hasCN: '繁体汉化补丁',
         link: 'https://keylol.com/t464503-1-1' 
     },
+    { 
+        en: 'Pillars of Eternity', 
+        cn: '永恒之柱', 
+        cover: 'https://media.st.dl.pinyuncloud.com/steam/apps/291650/header.jpg?t=1589873307',
+        source: '其乐社区',
+        hasCN: '汉化补丁',
+        link: 'https://keylol.com/t334498-1-1' 
+    },
+    { 
+        en: 'Alien: Isolation', 
+        cn: '异形隔离', 
+        cover: 'https://media.st.dl.pinyuncloud.com/steam/apps/214490/header_alt_assets_0.jpg?t=1602262841',
+        source: '其乐社区',
+        hasCN: '汉化补丁',
+        link: 'https://keylol.com/t527436-1-2' 
+    },
 ]
