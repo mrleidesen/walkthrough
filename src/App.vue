d39a6298a33da6e070e8edf70fa3cd8f1aec85cd
mrleidesen <mrleidesen@outlook.com>
2020-10-14 16:17:49 +0800

    feat: add search

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <v-app>
-        <v-app-bar app color="primary" dark>
-            <v-toolbar-title>WalkThrough</v-toolbar-title>
+        <v-app-bar 
+            dense 
+            app 
+            color="primary" 
+            dark
+        >
+            <v-toolbar-title class="subtitle-1">走穿-游戏攻略收集</v-toolbar-title>
         </v-app-bar>
 
         <v-btn
@@ -10,16 +15,29 @@
             bottom
             right
             color="primary"
+            @click="sheet = !sheet"
         >
             <v-icon>mdi-magnify</v-icon>
         </v-btn>
+        <v-bottom-sheet v-model="sheet">
+            <v-sheet height="150px">
+                <v-row justify="center">
+                    <v-col cols="8">
+                        <v-text-field 
+                            v-model="search"
+                            label="搜索"
+                            clearable
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+            </v-sheet>
+        </v-bottom-sheet>
 
         <v-main>
             <v-container fluid>
-                
-                <v-row>
+                <v-row v-show="getFilterList.length > 0">
                     <v-col 
-                        v-for="item in list"
+                        v-for="item in getFilterList"
                         :key="item.en"
                         cols="12"
                         sm="6" 
@@ -37,11 +55,15 @@
                                 <v-card-title class="subtitle-1">{{ item.cn }}</v-card-title>
                             </v-img>
                             <v-card-text>
+                                <p class="mb-1">游戏名：{{ `${item.cn} ${item.en}` }}</p>
                                 <p class="mb-1">作者：{{ item.author }}</p>
                                 <p class="mb-1">来源：{{ item.source }}</p>
                             </v-card-text>
                         </v-card>
                     </v-col>
+                </v-row>
+                <v-row v-show="getFilterList.length === 0" align="center" justify="center">
+                    <v-col cols="12" class="text-center">未找到相关的内容</v-col>
                 </v-row>
             </v-container>
         </v-main>
@@ -55,14 +77,25 @@ export default {
     name: 'App',
 
     data: () => ({
-        list: [...WALK_THROUGH]
+        list: [...WALK_THROUGH],
+        search: '',
+        sheet: false
     }),
+
+    computed: {
+        getFilterList() {
+            const {search, list} = this;
+            if (search) {
+                return list.filter(item => item.cn.match(search) || item.en.match(search));
+            }
+            return list;
+        }
+    },
 };
 </script>
 
 <style>
-.app-main {
-    position: relative;
-    padding-bottom: 30px;
+.v-image {
+    background-color: #333;
 }
 </style>
--- src/mock/index.js
+++ src/mock/index.js
@@ -1,3 +1,3 @@
 export const WALK_THROUGH = [
-    { en: 'Dark Souls', cn: '黑暗之魂', cover: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', author: 'test', source: 'test', link: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+    { en: 'Dark Souls', cn: '黑暗之魂', cover: '', author: 'test', source: 'test', link: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
 ]
