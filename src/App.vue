700ccabe49683c85e02392c59b68bc9b22b88f22
mrleidesen <mrleidesen@outlook.com>
2020-10-14 14:49:58 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <v-toolbar-title>WalkThrough</v-toolbar-title>
+        </v-app-bar>
+
+        <v-btn
+            fixed
+            fab
+            bottom
+            right
+            color="primary"
+        >
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-main>
+            <v-container fluid>
+                
+                <v-row>
+                    <v-col 
+                        v-for="item in list"
+                        :key="item.en"
+                        cols="12"
+                        sm="6" 
+                        md="3"
+                    >
+                        <v-card
+                            :href="item.link"
+                            target="blank"
+                        >
+                            <v-img
+                                class="white--text align-end"
+                                height="150px"
+                                :src="item.cover"
+                            >
+                                <v-card-title class="subtitle-1">{{ item.cn }}</v-card-title>
+                            </v-img>
+                            <v-card-text>
+                                <p class="mb-1">作者：{{ item.author }}</p>
+                                <p class="mb-1">来源：{{ item.source }}</p>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { WALK_THROUGH } from './mock';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+
+    data: () => ({
+        list: WALK_THROUGH
+    }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app-main {
+    position: relative;
+    padding-bottom: 30px;
 }
 </style>
---
+++ src/mock/index.js
@@ -0,0 +1,3 @@
+export const WALK_THROUGH = [
+    { en: 'Dark Souls', cn: '黑暗之魂', cover: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', author: 'test', source: 'test', link: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg' },
+]
