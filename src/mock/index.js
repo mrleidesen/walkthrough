29629ca26c6d7aa0691e147038622813e43fbc44
Mr.LeiDeSen <mrleidesen@outlook.com>
2020-10-16 09:34:05 +0800

    fix silent hill 4's cover and add game Black Mesa

--- src/mock/index.js
+++ src/mock/index.js
@@ -32,14 +32,22 @@ export const WALK_THROUGH = [
         cover: 'https://media.st.dl.pinyuncloud.com/steam/apps/214490/header_alt_assets_0.jpg?t=1602262841',
         source: '其乐社区',
         hasCN: '汉化补丁',
-        link: 'https://keylol.com/t527436-1-2' 
+        link: 'https://keylol.com/t527436-1-1' 
     },
     { 
         en: 'Silent Hill 4 The Room', 
         cn: '寂静岭4闭室', 
-        cover: 'http://tiebapic.baidu.com/forum/w%3D580/sign=41b8f95d239b033b2c88fcd225cf3620/c93a510fd9f9d72af5cbf444c32a2834359bbbb7.jpg',
+        cover: 'https://blob.keylol.com/forum/202009/01/193626hsu5pkkicotuswut.jpg',
         source: '百度贴吧',
         hasCN: '汉化补丁',
         link: 'https://tieba.baidu.com/p/6920265392' 
+    },
+    { 
+        en: 'Black Mesa', 
+        cn: '黑山起源', 
+        cover: 'https://steamcdn-a.akamaihd.net/steam/apps/362890/header.jpg?t=1593070451',
+        source: '其乐社区',
+        hasCN: '汉化补丁',
+        link: 'https://keylol.com/t130226-1-1' 
     },
 ]
